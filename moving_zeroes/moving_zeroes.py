282ffbcf987fa4fd4c58f2dc1bd0470a36b5e55b
Brian Wardwell <bwardwell79@gmail.com>
2020-07-22 16:05:08 -0700

    finshed first problem

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -3,7 +3,18 @@ Input: a List of integers
 Returns: a List of integers
 '''
 def moving_zeroes(arr):
-    # Your code here
+    # loop through arr starting at index 1
+    pointer = 0
+    for x in range(leng(arr) - 1):
+
+    # if int is greater than 0, insert on left side of array
+        if x > 0:
+            arr.insert(arr[pointer], x)
+        # if int is not greater than 0 move to next index
+        else:
+            pointer += 1
+    # return mutated arr after last index has been checked
+
 
     pass
 
--- single_number/single_number.py
+++ single_number/single_number.py
@@ -4,12 +4,35 @@ Returns: an integer
 '''
 def single_number(arr):
     # Your code here
+    # sort array
+    sort_arr(arr)
+    # create pointer at index 0 then compare index 0 to index 1
+    pointer = 0
+    # loop through arr
+    for x in range(pointer, len(arr) - 1):
+        # if index[0] equals index[1], at 2 to pointer and repeat
+        if arr[pointer] == arr[pointer + 1]:
+            pointer += 2
+        # if indexes don't match return first index in the comparison
+        else:
+            return x
+    
 
-    pass
-
+def sort_arr(arr):
+    # loop through n-1 elements
+    for i in range(0, len(arr) - 1):
+        cur_index = i
+        smallest_index = cur_index
+        for j in range(cur_index + 1, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+        arr[smallest_index], arr[cur_index] = arr[cur_index], arr[smallest_index]
+    return arr
 
 if __name__ == '__main__':
     # Use the main function to test your implementation
     arr = [1, 1, 4, 4, 5, 5, 3, 3, 9, 0, 0]
 
     print(f"The odd-number-out is {single_number(arr)}")
+
+    
